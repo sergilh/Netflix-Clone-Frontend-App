cef692848062d115bb1cb8f22b6ed89d58925169
Sergi López Hernández <serkio.lh@gmail.com>
2025-05-19 19:18:37 +0200

    Login definitivo terminado (parte de front, y devuelve el objeto con email y pasword por consola)

--- netflix-clone/app/(routes)/(auth)/login/LoginForm/FormError/FormError.tsx
+++ netflix-clone/app/(routes)/(auth)/login/LoginForm/FormError/FormError.tsx
@@ -1,5 +1,12 @@
-export function FormError() {
+import { FormErrorProps } from "./FormError.types";
+import { TriangleAlert } from "lucide-react";
+export function FormError(props: FormErrorProps) {
+    const { message } = props;
+    if (!message) return null;
     return (
-        <div>FormError</div>
+    <div className="bg-red-600/50 p-3 rounded flex items-center gap-x-2 text-sm text-white">
+        <TriangleAlert />
+            <p>{message}</p>
+    </div>
     )
 }
