3d06548acf61253274347db86fb31e0ecc1af75d
Sergi López Hernández <serkio.lh@gmail.com>
2025-05-19 18:12:08 +0200

    Estructura del login hecha

--- netflix-clone/app/(routes)/(auth)/layout.tsx
+++ netflix-clone/app/(routes)/(auth)/layout.tsx
@@ -1,17 +1,23 @@
+import { Logo } from "@/components/Shared/Logo";
 
-export default function RootLayout({
+export default function AuthLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
-      <html lang="es">
-        <body
-          className={`  bg-zinc-900`}
-            >
-                <h1>AuthLayout</h1>
-          {children}
-        </body>
-      </html>
+      <div className="h-full">
+        <div className="h-full relative">
+          <div className="bg-black h-full min-h-screen absolute w-full -z-10">
+            <div className="bg-[url('/login-bg.jpg')] h-full opacity-40" />
+            </div>
+            <div className="px-8 py-5 max-w-7xl mx-auto">
+              <Logo/>
+          </div>
+          <div className="h-full w-full max-w-md mx-auto">
+            <div className="bg-black/70 px-14 py-16">{children}</div>
+          </div>
+          </div>
+          </div>
     );
   }
---
+++ netflix-clone/app/(routes)/(auth)/login/page.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import { Checkbox } from "@/components/ui/checkbox"
+
+export default function LoginPage(){
+    return (
+        <div>
+            <p className='text-3xl font-bold text-left mb-7'>Iniciar sesión</p>
+            <p>Form Login....</p>
+            <div className='mt-5 text-center'>
+                <Link href="/" className='hover:underline hover:opacity-70'>
+                    ¿Has olvidado tu contraseña?
+                </Link>
+            </div>
+            <div className='flex items-center space-x-2 mt-4'>
+                <Checkbox id='terms' className='border-white' />
+                <label className='peer-disabled:cursor not allowed peer-disabled:opacity-70'>
+                    Recuérdame
+                </label>
+            </div>
+
+            <div className='mt-4 flex gap-3'>
+                <p className='text-white opacity-70'>¿Todavía sin Serflix?</p>
+                <Link href="/register" className=' text-white'>
+                    Suscríbete ahora!
+                </Link>
+            </div>
+        </div>
+    )
+}
