40ddc442ee772425d790f89a76a1fb766b406d37
Sergi López Hernández <serkio.lh@gmail.com>
2025-05-22 18:00:54 +0200

    Callbacks de inicio de sesion jwt y recuperación de token y asignacion de token a id correcto

--- netflix-clone/auth.ts
+++ netflix-clone/auth.ts
@@ -4,6 +4,19 @@ import { db } from "./lib/db"
 import authConfig from "./auth.config"
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id=token.sub
+      }
+      return session
+
+    },
+    async jwt({ token }) {
+      console.log({token})
+      return token
+    },
+  },
   session: { strategy: 'jwt' },
   ...authConfig,
 })
