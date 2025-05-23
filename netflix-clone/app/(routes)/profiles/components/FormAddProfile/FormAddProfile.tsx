0f7a4eeaaeca0c08ccf7e4a43301f3098f53e89b
Sergi López Hernández <serkio.lh@gmail.com>
2025-05-23 16:54:33 +0200

    Frontend de pagina de creacion de perfil con formulario de shadCN y radiogroup

--- netflix-clone/app/(routes)/profiles/components/FormAddProfile/FormAddProfile.tsx
+++ netflix-clone/app/(routes)/profiles/components/FormAddProfile/FormAddProfile.tsx
@@ -9,7 +9,6 @@ import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -17,6 +16,9 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { formSchema } from "./FormAddProfile.form";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { dataProfilesImages } from "./FormAddProfile.data";
+import Image from "next/image";
  
 
 
@@ -39,16 +41,44 @@ export default function FormAddProfile(props: FormAddProfileProps) {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="profileName"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Nombre de perfil</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="Juan" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+                />
+                <FormField
+          control={form.control}
+          name="avatarUrl"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Selecciona tu imagen de perfil</FormLabel>
+                  <FormControl>
+                  <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex  space-y-1"
+                      >
+                          {dataProfilesImages.map((image) => {
+                              return (
+                          <FormItem key={image.urlImage} className="flex flex-col-reverse justify-center items-center space-x-5 space-y-0 cursor-pointer">
+                                      <FormControl className="text-white ml-3 mt-1">
+                                          <RadioGroupItem value={image.urlImage}/>
+                                      </FormControl>
+                                      <FormLabel className="font-normal flex justify-center w-full">
+                                          <Image src={image.urlImage} alt="imageProfile" width={100} height={100} className={field.value === image.urlImage ? "cursor-pointer border-white":""} />
+                                      </FormLabel>
+                            </FormItem>
+                                  
+                              )
+                          })}
+                </RadioGroup>
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
